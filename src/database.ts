d17ce566d183ad1b4773cb84e880593611e1ce37
joaogabriellyra <jgabriellyra@hotmail.com>
2024-08-20 21:33:52 -0300

    created the temp db

--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,6 @@ import { knex as setupKnex } from 'knex'
 export const knex = setupKnex({
   client: 'sqlite',
   connection: {
-    host: 'localhost',
-    port: 3306,
-    user: 'user_db',
-    password: 'password_db',
-    database: 'ignite-node',
+    filename: './tmp/app.db',
   },
 })
